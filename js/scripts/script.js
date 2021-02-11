3b55017800cf6dd219d8da4a93c6b48f04e1524c
SunnyMeghwal <rinkumeghwal@gmail.com>
2021-02-11 16:03:24 +0530

    class ho gyayi aaj  ki

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,5 +1,36 @@
-//alert("hello world");
-//console.log("jai shree ram");
-//document.write('hello world');
-document.getElementById("demo").innerHTML
- 
+/*var abc = "hello world"
+var abc = ["hello world", "hello ji",2,3,6,8,9,1,];
+abc[0] 
+p = document.getElementsByTagName('p')
+p[0].className = 'hello'
+
+for(i = 1; i < 100; i++)
+{
+    if(i % 5 == 0)
+    {
+        document.write('<h2>' + i + '</h2>' + ' is even <br/>')
+    }
+    else
+    {
+        document.write(i + ' is odd <br/>')
+    }
+
+}
+*/
+
+var person_id = [1,2,3,4,5,6,7,8,9,10]
+var person_name = ['Sanjay','Shubham','Ritik','Rohan','Praveen','Jeetu','Prithvi','Ram','Shyam','Ritik ki gf']
+
+document.write('<table><tr><td>Person id</td><td>Person Name</td></tr>');
+for(i=0; i < person_id.length; i++){
+    if(!(i % 2 == 0))
+    {
+        document.write('<tr><td class="red">'+ person_id[i] +'</td><td class="red">'+ person_name[i] +'</td></tr>');
+    }
+   else {
+        document.write('<tr><td>'+ person_id[i] +'</td><td>'+ person_name[i] +'</td></tr>');
+    }
+
+}
+
+document.write('</table>')
